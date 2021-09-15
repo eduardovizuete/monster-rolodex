b95c6009f2df2d92d56e56f55ff24c21a1049c1d
edu <gilberto.vizuete@modinter.com.ec>
2021-09-15 12:28:52 -0500

    Fetching Content

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 import { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          id: 'mfra',
-          name: 'Frankenstein'
-        },
-        {
-          id: 'mdra',
-          name: 'Dracula'
-        },
-        {
-          id: 'mzom',
-          name: 'Zombie'
-        }
-      ]
+      monsters: []
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
