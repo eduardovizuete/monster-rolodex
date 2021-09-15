5b5b4f104fa156b9df31ab7f6e8cf39ff467edd8
edu <gilberto.vizuete@modinter.com.ec>
2021-09-15 13:03:46 -0500

    Card List Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -19,11 +20,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monsters => (
-            <h1 key={monsters.id}> {monsters.name} </h1>
-          ))
-        }
+        <CardList name="Edu"> 
+          {
+            this.state.monsters.map(monsters => (
+              <h1 key={monsters.id}> {monsters.name} </h1>
+            ))
+          }
+        </CardList>
       </div>
     )
   }
